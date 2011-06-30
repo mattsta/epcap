4452d85cc51accc443f8a9369476439948dd1d00
Matt Stancliff <matt@genges.com>
2011-06-29 17:47:31 -0700

    Remove wireless support
    Older versions of libpcap can't compile with rfmon

--- c_src/epcap.c
+++ c_src/epcap.c
@@ -71,9 +71,6 @@ main(int argc, char *argv[])
             case 'i':
                 IS_NULL(ep->dev = strdup(optarg));
                 break;
-            case 'M':
-                ep->rfmon = 1;
-                break;
             case 'P':
                 ep->promisc = 1;
                 break;
@@ -154,10 +151,6 @@ epcap_open(EPCAP_STATE *ep)
             PCAP_ERRBUF(ep->dev = pcap_lookupdev(errbuf));
 
         PCAP_ERRBUF(ep->p = pcap_open_live(ep->dev, ep->snaplen, ep->promisc, ep->timeout, errbuf));
-
-        /* monitor mode */
-        if (pcap_can_set_rfmon(ep->p) == 1)
-            (void)pcap_set_rfmon(ep->p, ep->rfmon);
     }
 
     return (0);
@@ -296,7 +289,6 @@ usage(EPCAP_STATE *ep)
             "              -d <directory>   chroot directory\n"
             "              -i <interface>   interface to snoop\n"
             "              -f <filename>    read from file instead of live capture\n"
-            "              -M               wireless monitor (rfmon) mode\n"
             "              -P               promiscuous mode\n"
             "              -g <group>       unprivileged group\n"
             "              -u <user>        unprivileged user\n"
